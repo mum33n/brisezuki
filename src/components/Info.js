b3bf19d6c5ecaf53a4eb5861a59b70d49040785e
mum33n <azeezmumeen6@gmail.com>
2022-05-20 17:24:31 +0100

    info update

--- src/components/Info.js
+++ src/components/Info.js
@@ -16,13 +16,13 @@ export default function Info(params) {
             <FeaturesCard img={acad} title={'Zuki Academy'}/>
         </div>
     </div>
-    <div className="text-slate-100 py-20 px-10 wow fadeInUp" data-wow-duration="2s">
-        <h1 className="text-3xl md:w-2/3 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Is Zuki Inu Built On Bitgert Chain</h1>
-        <p className="pt-5 md:w-2/3 mx-auto md:text-2xl wow fadeInUp" data-wow-duration="2s">Bitgert chain projects recently have been sparking massive attention from the crypto world; this is due to the increasing developments around the bitgert chain projects. They have recorded one of the fastest growths since it was launched.</p>
-        <p className="pt-5 md:w-2/3 mx-auto md:text-2xl wow fadeInUp" data-wow-duration="2s">
-        Bitgert chain projects are powered with the Bitgert blockchain which is the fastest and first gasless chain in the crypto industry. This indicates that Bitgert chain projects operate with a cheaper and faster transaction rate compared to other cryptocurrencies. Furthermore, the adoption of the Bitgert chain has grown rapidly because of its zero gas fee mechanism. BRISE chain which does 100,000 within a second and the transaction fee is $0.00000001.
-        </p>
-        <a href="/" className=" bg-slate-800 mt-3 mx-auto text-center md:w-1/3 w-1/2 block shadow-lg py-3 px-5 text-slate-100 rounded-full">Bitgert</a>
+    <div className="text-slate-100 pb-20 px-10 wow fadeInUp" data-wow-duration="2s">
+        <h1 className="text-3xl md:w-2/3 mb-10 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Is Zuki Inu Built On Bitgert Chain</h1>
+        <div className="flex justify-between">
+            <FeaturesCard title={'Fast & Scalable'} exp={'100,000 Transactions per second & 15s block'}/>
+            <FeaturesCard title={'Decentralized'} exp={'Fully decentralized blockchain with high-speed transactions'}/>
+            <FeaturesCard title={'Zero Gas Fee'} exp={'Zero cost gas fee for all the transactions on our blockchain'}/>
+        </div>
     </div>
         </div>
     )
--- src/components/cards/FeaturesCard.js
+++ src/components/cards/FeaturesCard.js
@@ -1,11 +1,12 @@
-export default function FeaturesCard({img, title}) {
+export default function FeaturesCard({img, title, exp}) {
     return(
         <div className="text-slate-100 text-center cursor-pointer mb-10 hover:bg-slate-900 bg-shade w-full md:w-30 px-3 py-10  box ">
-                <div className="bg-slate-100 mb-5 mx-auto p-2 rounded-full" style={{width:'70px'}}>
+                <div className="bg-slate-100 mb-5 mx-auto p-3 rounded-full" style={{width:'70px'}}>
                     <img className="block" src={img}/>
                 </div>
                 <div>
-                    <h1>{title}</h1>
+                    <h1 className="font-bold mb-2">{title}</h1>
+                    <p>{exp}</p>
                 </div>
         </div>
     )
