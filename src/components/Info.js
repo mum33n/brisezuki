8f666aa8703a18ad0325d9f2865fd089ccafdcd3
mum33n <azeezmumeen6@gmail.com>
2022-05-20 17:27:03 +0100

    info style update

--- src/components/Info.js
+++ src/components/Info.js
@@ -18,7 +18,7 @@ export default function Info(params) {
     </div>
     <div className="text-slate-100 pb-20 px-10 wow fadeInUp" data-wow-duration="2s">
         <h1 className="text-3xl md:w-2/3 mb-10 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Is Zuki Inu Built On Bitgert Chain</h1>
-        <div className="flex justify-between">
+        <div className="flex flex-row gap-5 flex-wrap justify-between">
             <FeaturesCard title={'Fast & Scalable'} exp={'100,000 Transactions per second & 15s block'}/>
             <FeaturesCard title={'Decentralized'} exp={'Fully decentralized blockchain with high-speed transactions'}/>
             <FeaturesCard title={'Zero Gas Fee'} exp={'Zero cost gas fee for all the transactions on our blockchain'}/>
