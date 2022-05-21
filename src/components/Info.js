101a58c55e16e7519af21f98b793c62350a859e2
mum33n <azeezmumeen6@gmail.com>
2022-05-21 21:24:48 +0100

    links fixed

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,8 @@ export default function Info(params) {
     return(
         <div id='why'>
             <div className="text-slate-100 py-20 px-10 wow fadeInUp" data-wow-duration="2s">
-        <h1 className="text-3xl mb-10 md:w-2/3 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Zuki Brise?</h1>
+        <h1 className="text-3xl mb-5 md:w-2/3 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Zuki Brise?</h1>
+        <h1 className="text-center mb-10 font-bold">'Charitable Mindset' differentiates Zuki from other meme projects on Bitgert</h1>
         <div className="flex flex-row gap-5 flex-wrap justify-between">
             <FeaturesCard img={airdrop} title={'Airdrop Dispenser'}/>
             <FeaturesCard img={market} title={'NFT Marketplace'}/>
@@ -17,7 +18,7 @@ export default function Info(params) {
         </div>
     </div>
     <div className="text-slate-100 pb-20 px-10 wow fadeInUp" data-wow-duration="2s">
-        <h1 className="text-3xl md:w-2/3 mb-10 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Is Zuki Brise Built On Bitgert Chain</h1>
+        <h1 className="text-3xl md:w-2/3 mb-10 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Is Zuki Brise Built On Bitgert Chain?</h1>
         <div className="flex flex-row gap-5 flex-wrap justify-between">
             <FeaturesCard title={'Fast & Scalable'} exp={'100,000 Transactions per second & 15s block'}/>
             <FeaturesCard title={'Decentralized'} exp={'Fully decentralized blockchain with high-speed transactions'}/>
