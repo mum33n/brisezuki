e73005d3673444c9569da678fa071d56245edaab
mum33n <azeezmumeen6@gmail.com>
2022-05-21 22:24:15 +0100

    links fixed

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import airdrop from '../assets/airdrop2.png'
 import market from '../assets/market.png'
 import game from '../assets/game.png'
