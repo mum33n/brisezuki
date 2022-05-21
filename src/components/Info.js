5c86574062bf215b67e39eda7d08b55da4628d44
mum33n <azeezmumeen6@gmail.com>
2022-05-21 22:38:35 +0100

    links fixed

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,7 @@ export default function Info(params) {
         <div id='why'>
             <div className="text-slate-100 py-20 px-10 wow fadeInUp" data-wow-duration="2s">
         <h1 className="text-3xl mb-5 md:w-2/3 mx-auto md:text-6xl text-center font-bold wow fadeInUp" data-wow-duration="2s">Why Zuki Brise?</h1>
-        <h1 className="text-center mb-10 font-bold wow fadeInUp">'Charitable Mindset' differentiates Zuki from other meme projects on Bitgert</h1>
+        <h1 className="text-center mb-10 font-bold wow fadeInUp">'Charitable Mindset and Academy' differentiates Zuki from other meme projects on Bitgert</h1>
         <div className="flex flex-row gap-5 flex-wrap justify-between">
             <FeaturesCard img={airdrop} title={'Airdrop Dispenser'}/>
             <FeaturesCard img={market} title={'NFT Marketplace'}/>
